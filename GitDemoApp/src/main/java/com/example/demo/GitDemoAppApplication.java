383a7240f915eea44e234457f0073a809b063695
swetha42nama <107468270+swetha42nama@users.noreply.github.com>
2022-06-14 16:24:57 +0530

    Update GitDemoAppApplication.java

--- GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
+++ GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoAppApplication {
 
 	public static void main(String[] args) {
+	System.out.println("wel");
 		SpringApplication.run(GitDemoAppApplication.class, args);
 	}
 
